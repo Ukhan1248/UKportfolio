63085e64ddbe532c0e5dbc94bb13b1f00fe3ccd0
Umair Khan <umairkhan814@icloud.com>
2022-03-08 12:41:33 -0600

    Add files via upload

--- frontend_react/src/index.js
+++ frontend_react/src/index.js
@@ -1,6 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+//import NavigationDots from "./NavigationDots";
+//mport SocialMedia from "./SocialMedia";
+export { default as Navbar } from "./Navbar/Navbar";
