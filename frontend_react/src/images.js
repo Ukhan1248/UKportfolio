e842673fcba7db1af0c09f1b22db0be6433f9849
Umair Khan <umairkhan814@icloud.com>
2022-04-06 20:28:58 -0500

    Add files via upload

--- frontend_react/src/images.js
+++ frontend_react/src/images.js
@@ -18,7 +18,6 @@ import sass from "../assets/sass.png";
 import typescript from "../assets/typescript.png";
 import vue from "../assets/vue.png";
 
-import about01 from "../assets/about01.png";
 import about02 from "../assets/about02.png";
 import about03 from "../assets/about03.png";
 import about04 from "../assets/about04.png";
@@ -55,7 +54,6 @@ export default {
   sass,
   typescript,
   vue,
-  about01,
   about02,
   about03,
   about04,
